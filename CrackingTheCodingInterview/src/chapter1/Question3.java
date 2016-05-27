e306eeaa94cb9bca2e35c49ba4d4c27753a3bdab
Marcel Rebouças <mscreboucas@gmail.com>
2016-05-26 23:50:33 -0300

    Question 3

---
+++ CrackingTheCodingInterview/src/chapter1/Question3.java
@@ -0,0 +1,45 @@
+package chapter1;
+
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * Given two strings, write a method to decide if one is a 
+ * permutation of the other.
+ */
+public class Question3 {
+	
+	static boolean isPermutation(String str1, String str2) {
+
+		Map<Integer, Integer> charMap1 = generateCharMap(str1);
+		Map<Integer, Integer> charMap2 = generateCharMap(str2);
+
+		return charMap1.equals(charMap2);
+	}
+
+	static Map<Integer, Integer> generateCharMap (String str) {
+
+		Map<Integer, Integer> charMap = new HashMap<Integer, Integer>();
+
+		for (int i = 0; i < str.length(); i++) {
+			
+			int value = str.charAt(i);
+			Integer mappedValue = charMap.get(value);
+			
+			if (mappedValue == null) {
+				charMap.put(value, 1);
+			} else {
+				charMap.put(value, mappedValue+1);
+			}
+		}
+		return charMap;
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(isPermutation("aba", "aab"));
+		System.out.println(isPermutation("a", "aa"));
+		System.out.println(isPermutation("abaac", "aabca"));
+		System.out.println(isPermutation("abaqwe", "qweazzz"));
+		
+	}
+}
