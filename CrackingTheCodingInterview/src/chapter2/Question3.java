68d5e0a1ec93216724d3f8afb00d03e983e96ade
Marcel Rebouças <mscreboucas@gmail.com>
2016-06-01 21:03:12 -0300

    Question 2.3 and 2.4

---
+++ CrackingTheCodingInterview/src/chapter2/Question3.java
@@ -0,0 +1,37 @@
+package chapter2;
+
+/**
+ * Implement an algorithm to delete a node in the middle 
+ * of a singly linked list, given only access to that node.
+ *
+ */
+public class Question3 {
+
+
+	static void deleteNode (Node<Integer> n) {
+		
+		if (n == null) { return; }
+		
+		if (n.next != null) {
+			n.value = n.next.value;
+			n.next = n.next.next;
+		} 
+	}
+	
+	public static void main(String[] args) {
+		Node<Integer> n = new Node<Integer>(1);
+		n.addToTail(2);
+		n.addToTail(3);
+		n.addToTail(4);
+		n.addToTail(5);
+		System.out.println(n.toString());
+		
+		deleteNode(n);
+		System.out.println(n.toString());
+		
+		Node<Integer> middleNode = n.next.next;
+		deleteNode(middleNode);
+		System.out.println(n.toString());
+	}
+
+}
---
+++ CrackingTheCodingInterview/src/chapter2/Question4.java
@@ -0,0 +1,71 @@
+package chapter2;
+
+/**
+ * Write code to partition a linked list around a value x,
+ * such that all nodes less than x come before alt nodes 
+ * greater than or equal to x.
+ *
+ */
+public class Question4 {
+
+	static Node<Integer> partitionList(Node<Integer> node, int k) {
+
+		if (node == null) { return null; }
+
+		Node<Integer> headLesser = null;
+		Node<Integer> headHigher = null;
+
+		Node<Integer> lastLesserNode = null;
+		Node<Integer> lastHigherNode= null;
+
+		Node<Integer> newNode;
+
+		while (node != null) {
+
+			newNode = new Node<Integer>(node.value);
+
+			if (node.value < k){
+
+				if (headLesser == null) { 
+					headLesser = newNode;
+					lastLesserNode = headLesser;
+				} else {
+					lastLesserNode.next = newNode;
+					lastLesserNode = newNode;
+				}
+
+			} else {
+
+				if (headHigher == null) { 
+					headHigher = newNode;
+					lastHigherNode = headHigher;
+				} else {
+					lastHigherNode.next = newNode;
+					lastHigherNode = newNode;
+				}
+			}
+			
+			node = node.next;
+		}
+		
+		if (headLesser == null) {
+			return headHigher;
+		} else {
+			lastLesserNode.next = headHigher;
+			return headLesser;
+		}
+	}
+public static void main(String[] args) {
+	Node<Integer> n = new Node<Integer>(9);
+	n.addToTail(4);
+	n.addToTail(3);
+	n.addToTail(7);
+	n.addToTail(54);
+	n.addToTail(32);
+	n.addToTail(1);
+	
+	Node<Integer> partitioned = partitionList(n, 6);
+	System.out.println(partitioned.toString());
+}
+
+}
